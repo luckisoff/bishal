6efe96dd020797d690b38ddf7636f5c2c41bd623
luckisoff <safifalak@gmail.com>
2020-02-14 23:01:24 +0545

    update

--- app/Http/Controllers/Admin/DashBoardController.php
+++ app/Http/Controllers/Admin/DashBoardController.php
@@ -5,16 +5,23 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\UserPost;
+use App\Models\Hotel;
+use App\Models\Category;
 
 class DashBoardController extends BaseAdminController
 {
+    protected $limit;
     public function __construct()
     {
         parent::__construct();
+        $this->limit = 10;
     }
-    
+
     public function dashboard()
     {
+        $dashboard = new Self();
         $user = auth()->user();
 
         if($user->hasRole('manager'))
@@ -27,7 +34,40 @@ class DashBoardController extends BaseAdminController
             Auth::logout();
             return redirect()->back()->withErrors("Access denied",'error');;
         }
-        
-        return view('admin.parts.index');
+
+        return view('admin.parts.index',compact('dashboard'));
     }
+
+    // all latest
+
+    public function totalUsers()
+    {
+        return User::count();
+    }
+
+    public function recentUsers()
+    {
+        return User::orderBy('updated_at','desc')->limit($this->limit)->get();
+    }
+
+    public function newUsers()
+    {
+        return User::orderBy('created_at','desc')->limit($this->limit)->get();
+    }
+
+    public function posts()
+    {
+        return UserPost::orderBy('created_at','desc')->limit($this->limit)->get();
+    }
+
+    public function totalHotels()
+    {
+        return Hotel::count();
+    }
+
+    public function totalCategory()
+    {
+        return Category::count();
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,6 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 Route::group(['prefix'=>'v1'],function(){
     Route::post('login', 'Api\UserController@login');
     Route::post('register', 'Api\UserController@signup');
