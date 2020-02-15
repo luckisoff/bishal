1e51cae23650b3e2f983420caf74b75d6569a0ba
luckisoff <safifalak@gmail.com>
2020-02-15 22:26:17 +0545

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-	<title>Dashboard | Melon - Flat &amp; Responsive Admin Template</title>
+	<title>Dashboard | {{Site::setting('name')}}</title>
 
 	<!--=== CSS ===-->
 
