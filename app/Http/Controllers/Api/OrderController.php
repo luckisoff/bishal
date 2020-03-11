6672ad0c4c5fac2c81a3c5a921fe6c47427cb441
luckisoff <safifalak@gmail.com>
2020-03-11 11:03:56 +0545

    changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -147,12 +147,12 @@ class OrderController extends BaseApiController
                             return Carbon::parse($date->created_at)->format('Y-m-d');
                         });
             
-            $neworders = array();
+            $neworders[] = array();
 
             foreach($orders as $date => $items)
             {
-                $neworders[]['date'] = $date;
-                $neworders[]['items'] = $items;
+                $neworders['date'] = $date;
+                $neworders['items'] = $items;
             }
             return $this->successResponse(['orders'=>$neworders],'Users order listing');
         } catch (\Throwable $th) {
