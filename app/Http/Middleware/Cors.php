b02e235e9aaf195ceef34ee5262077efdbfff643
semiPlanet <planetsafi@gmail.com>
2020-03-13 18:36:32 +0545

    cors middleware added

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,6 @@ class Cors
     {
         return $next($request)
                     ->header("Access-Control-Allow-Origin", "*")
-                    ->header("Access-Control-Allow-Methods", "Get, Post, Put, Delete, Options")
-                    ->header("Access-Control-Allow-Headers", "*");
+                    ->header("Access-Control-Allow-Methods", "Get, Post, Put, Delete, Options");
                 }
 }
