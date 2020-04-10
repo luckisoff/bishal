fbb14989607c2370f5465bbed26fc9b5d593ae22
semiPlanet <planetsafi@gmail.com>
2020-04-10 20:04:45 +0545

    cors middleware added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        // \App\Http\Middleware\Cors::class,
+        \App\Http\Middleware\Cors::class,
 
     ];
 
@@ -40,9 +40,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
-            'bindings',
-        ],
+            'throttle:1500,1',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]
     ];
 
     /**
